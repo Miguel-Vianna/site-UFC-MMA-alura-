d4ba9d7a2d82b670cca01b25e9cf1eba49bdba75
Miguel-Vianna <miguelcvianna0407@gmail.com>
2024-09-09 19:30:15 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : 'Alex Pereira Poatan',
+        descricao : 'Alex Pereira, conhecido como Poatan, é um lutador brasileiro de artes marciais mistas (MMA) que vem conquistando o mundo dos esportes de combate. Com um histórico impressionante no kickboxing, onde foi campeão mundial em diversas categorias, Poatan trouxe sua potência e precisão para o MMA, rapidamente se tornando um dos nomes mais perigosos da divisão dos meio-pesados.',
+        link : "https://pt.wikipedia.org/wiki/Alex_Pereira",
+        tags: "poatan chama"
+    },
+    {
+        titulo : 'Charles Oliveira do Bronx',
+        descricao : 'Charles Oliveira, conhecido carinhosamente como "do Bronx", é um dos maiores lutadores de MMA da história. Nascido em Guarujá, São Paulo, o brasileiro conquistou o mundo das artes marciais mistas com seu jogo de chão impecável e sua incrível capacidade de finalizar seus adversários.',
+        link : "https://pt.wikipedia.org/wiki/Charles_Oliveira",
+        tags: "do bronx"
+    },
+    {
+        titulo : 'Lyoto Machida',
+        descricao : 'Lyoto Machida é um nome que ecoa no mundo das artes marciais mistas com a elegância e a eficiência de um samurai. Com um estilo de luta único, baseado no karatê tradicional, Machida revolucionou o MMA, introduzindo movimentos fluidos e imprevisíveis que deixavam seus adversários perplexos.',
+        link : "https://pt.wikipedia.org/wiki/Lyoto_Machida",
+        tags: "chines samurai"
+    }  
+    ];
